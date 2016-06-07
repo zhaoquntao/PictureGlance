29293abbee3d7d665ef69dc4443440f4eb1fd01b
赵群涛 <1343108587@qq.com>
2016-06-07 21:04:06 +0800

    Initial Commit

---
+++ 照片浏览Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  照片浏览Demo
+//
+//  Created by 赵群涛 on 16/6/7.
+//  Copyright © 2016年 赵群涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 照片浏览DemoTests/____DemoTests.m
@@ -0,0 +1,39 @@
+//
+//  ____DemoTests.m
+//  照片浏览DemoTests
+//
+//  Created by 赵群涛 on 16/6/7.
+//  Copyright © 2016年 赵群涛. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ____DemoTests : XCTestCase
+
+@end
+
+@implementation ____DemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
